d9b62053087c1bcdf363fbf055a32c5f0aec1a3d
usori <victoroliveiraprofile@gmail.com>
2020-12-13 19:07:51 -0300

    Primeiro commit, html e js

---
+++ calculadora.js
@@ -0,0 +1,12 @@
+var valor;
+function botao(num) {
+  document.calc.visor.value += num
+}
+
+function resetar(){
+  document.calc.visor.value = ''
+}
+
+function calcular(){
+  document.calc.visor.value = 'Eu amo a Carol <3'
+}
